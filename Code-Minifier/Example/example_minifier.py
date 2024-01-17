b5d5981cc2284646e4bd0faf9b44449d87b1b598
Ferly Afriliyan - Ryougaa <122451961+ferlyafriliyan@users.noreply.github.com>
2024-01-17 22:29:17 +0700

    Add files via upload

---
+++ Code-Minifier/Example/example_minifier.py
@@ -0,0 +1,9 @@
+# Take the encryption method from the website = https://pyobfuscate.com/minifier
+# Time : Wednesday, January 17, 2024 23:24:07
+# Platform : Windows-AMD64
+import os,sys
+def clear():
+	if'linux'in sys.platform.lower():os.system('clear')
+	elif'win'in sys.platform.lower():os.system('cls')
+def hello():A=', I am Ferly Afriliyan ! ';B='and I just finished creating a python project to Obfuscate Python3 files which is quite difficult to Reverse';print('Hello world',A);print(B)
+if __name__=='__main__':clear();hello()
